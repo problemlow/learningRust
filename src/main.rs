2ba2f655dd6f31eaabb252acd3da317771303cb5
problemlow <jordancool2009@hotmail.co.uk>
2023-04-06 19:36:38 +0100

    asd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+    let mut x = 4;
+    println!("x is: {}", x);
+    {
+        let x = 2;
+        println!("x is: {}", x);
+    }
+    x += 1;
+    println!("x is: {}", x);
 }
