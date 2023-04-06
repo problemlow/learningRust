b6025d305f973a745adbe6b3a32e183abf69e3b3
problemlow <jordancool2009@hotmail.co.uk>
2023-04-06 20:06:48 +0100

    adDidnt Set Commit Message

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 fn main() {
     println!("Hello, world!");
-    let mut x: String = meme;
-    println!("x is: {}", x);
-    x = x+"emem";
-    println!("x is: {}", x);
+    let mut x = 5;
+    println!("{}", x);
 }
